7841263c6603122a43e447ec4e555f891da5acf0
IslamelGebaly <islamAlgebaly11@gmail.com>
2022-12-31 22:21:54 +0200

    Base

--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,29 @@ function app(){
 
     //Text Field
     const title = document.createElement("input");
-    title.className = "title"
+    title.className = "title";
     inputContainer.appendChild(title);
 
     ///Set Reminder
     const reminder = document.createElement("input");
     inputContainer.appendChild(reminder);
 
+    //Submit Button
+    const submitButton = document.createElement("button");
+    submitButton.textContent = "+";
+    inputContainer.appendChild(submitButton);
+    
+    //TODOs field
+    const todoContainer = document.createElement("ul");
+    todoContainer.className = "todo";
+    document.body.appendChild(todoContainer);
+
+    ///ToDo Items
+    submitButton.addEventListener("click",() => {
+        listItem = document.createElement("li");
+        listItem.textContent = `${title.value}                      ${reminder.value}`
+        todoContainer.appendChild(listItem);
+    });
 
 }
 
