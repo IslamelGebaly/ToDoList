f22d5025b4bfb8663a3273514abc48f299de9fca
IslamelGebaly <islamAlgebaly11@gmail.com>
2023-01-02 18:11:41 +0200

    added done field

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,6 @@ function app(){
     ///Set Reminder
     const reminder = document.createElement("input");
     reminder.setAttribute("type", "time");
-    reminder.setAttribute("min", `${new Date().getHours()}:${new Date().getMinutes()}`);
     reminder.setAttribute("value", `${new Date().getHours()}:${new Date().getMinutes()}`);
     inputContainer.appendChild(reminder);
 
@@ -26,23 +25,52 @@ function app(){
     todoContainer.className = "todo";
     document.body.appendChild(todoContainer);
 
+    //Done field
+    const doneContainer = document.createElement("ul");
+    doneContainer.className = "todo";
+    document.body.appendChild(doneContainer);
+
     ///ToDo Items
     submitButton.addEventListener("click",() => {
-        listItem = document.createElement("li");
+        const listItem = document.createElement("li");
         listItem.textContent = `${title.value}\t${reminder.value}`;
 
         ///Done button for list it
         const doneButton = document.createElement("button");
         doneButton.textContent = "Done";
-        doneButton.addEventListener("click", () => {
-            todoContainer.removeChild( doneButton.parentElement);
-        });
+        doneButton.addEventListener("click",() => onDone(doneButton, todoContainer, doneContainer));
 
         listItem.appendChild(doneButton);
         
         todoContainer.appendChild(listItem);
     });
 
+}
+
+function onDone(doneButton, todoContainer, doneContainer){
+    const listItem = document.createElement("li");
+    listItem.textContent = doneButton.parentElement.childNodes[0].nodeValue;
+    
+    const returnButton = document.createElement("button");
+    returnButton.textContent = "Return";
+    returnButton.addEventListener("click", () => onReturn(returnButton, todoContainer, doneContainer));
+    listItem.appendChild(returnButton);
+    
+    doneContainer.appendChild(listItem);
+    todoContainer.removeChild( doneButton.parentElement);
+}
+
+function onReturn(returnButton, todoContainer, doneContainer){
+    const listItem = document.createElement("li");
+    listItem.textContent = returnButton.parentElement.childNodes[0].nodeValue;
+
+    const doneButton = document.createElement("button");
+    doneButton.textContent = "Done";
+    doneButton.addEventListener("click", () => onDone(doneButton, todoContainer, doneContainer));
+    listItem.appendChild(doneButton);
+
+    todoContainer.appendChild(listItem);
+    doneContainer.removeChild(returnButton.parentElement);
 }
 
 app();
